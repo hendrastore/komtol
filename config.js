9495c0b10202f4b7ced4e3ac5d2ce1b52f84ae3a
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-04 20:28:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Arull.
+   * Contact Me on wa.me/6285822347348
+   * Follow https://github.com/Arullwara
 */
 
 const fs = require('fs')
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6285822347348','6285822347348','6285822347348']
+global.premium = ['6285822347348']
+global.packname = 'Arull'
+global.author = 'ArullBotz'
+global.sessionName = 'Session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {

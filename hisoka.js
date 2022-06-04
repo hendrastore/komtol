521f74bed9889cb41a9ddc24e9defb0d6fc024b7
Arullwara <106657360+Arullwara@users.noreply.github.com>
2022-06-04 21:31:03 +0800

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create by Arull.
+   * Contact Me on wa.me/6285822347348
+   * Follow https://github.com/Arullwara
 */
 
 require('./config')
@@ -1316,7 +1316,8 @@ break
 	    break
 	       case 'attp': case 'ttp': {
            if (!text) throw `Example : ${prefix + command} text`
-           await hisoka.sendMedia(m.chat, `https://xteam.xyz/${command}?file&text=${text}`, 'hisoka', 'morou', m, {asSticker: true})
+           await hisoka.sendMedia(m.chat, `https://xteam.xyz/${command}?file&text=${text}`, 'hisoka', 'morou', m, {asSticker: true})
+
          }
          break
 	       case 'smeme': case 'stickmeme': case 'stikmeme': case 'stickermeme': case 'stikermeme': {
@@ -2724,8 +2725,8 @@ let capt = `⭔ Title: ${judul}
                 }
             }
             break
-            case 'list': case 'menu': case 'help': case '?': {
-                anu = `┌──⭓ *Group Menu*
+            case 'list': case 'menu': case 'help': case '?': { 
+             anu = `┌──⭓ *Group Menu*
 │
 │⭔ ${prefix}linkgroup
 │⭔ ${prefix}ephemeral [option]
@@ -3045,12 +3046,12 @@ let capt = `⭔ Title: ${judul}
                 let btn = [{
                                 urlButton: {
                                     displayText: 'Source Code',
-                                    url: 'https://github.com/DikaArdnt/Hisoka-Morou'
+                                    url: 'https://github.com/DikaArdnt/ArullWara'
                                 }
                             }, {
                                 callButton: {
                                     displayText: 'Number Phone Owner',
-                                    phoneNumber: '+62 882-9202-4190'
+                                    phoneNumber: '+6285822347348'
                                 }
                             }, {
                                 quickReplyButton: {
